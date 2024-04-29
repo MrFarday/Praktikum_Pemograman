253358af6a17d208f86f5f4534a1df33aed450cd
MrFarday <11jauhari11@gmail.com>
2024-04-29 21:17:53 +0700

    addHead & addMid

--- List/src/ListMain.java
+++ List/src/ListMain.java
@@ -2,10 +2,16 @@ public class ListMain {
     
     public static void main(String[] args) {
         StrukturList list = new StrukturList();
+        list.addHead(1);
+        list.addHead(5);
+        list.addHead(7);
+
         list.addTail(2);
         list.addTail(3);
         list.addTail(6);
         list.addTail(9);
+
+        list.addMid(4, 2);
 
         System.out.println("Elemen: ");
         list.displayElement();
--- List/src/StrukturList.java
+++ List/src/StrukturList.java
@@ -9,6 +9,32 @@ public class StrukturList {
         return HEAD == null;
     }
 
+    public void addMid(int data, int position) {
+        Node posNode = null;
+        Node curNode = null;
+        int i;
+        Node newNode = new Node(data);
+        if (HEAD == null) {
+            HEAD = newNode; // Tambah di awal jika linked list masih kosong
+        } else {
+            curNode = HEAD;
+            if (position == 1) {
+                newNode.setNext(curNode);
+                HEAD = newNode; // Tambah di awal
+            } else {
+                i = 1;
+                while (curNode != null && i < position) {
+                    posNode = curNode;
+                    curNode = curNode.getNext();
+                    i++;
+                }
+                posNode.setNext(newNode);
+                newNode.setNext(curNode);
+            }
+        }
+    }
+    
+
     public void addTail(int data){
         Node posNode = null;
         Node curNode = null;
@@ -25,6 +51,17 @@ public class StrukturList {
                 curNode = curNode.getNext();
             }
             posNode.setNext(newNode);
+        }
+    }
+
+    public void addHead(int data) {
+        Node newNode = new Node(data);
+        if (isEmpty()){
+            HEAD = newNode;
+        }
+        else {
+            newNode.setNext(HEAD);
+            HEAD = newNode;
         }
     }
 
